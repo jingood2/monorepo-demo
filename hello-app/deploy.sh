ed4f6b09c744985bb35deb60ab2f0c47dda5c99a
jingood2 <jingood2@gmail.com>
2022-05-11 20:18:30 +0900

    update deploy.sh

--- hello-app/deploy.sh
+++ hello-app/deploy.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 npm install -g serverless
-serverless deploy --stage $env --package $CODEBUILD_SRC_DIR/target/$env -v -r ap-northeast-2
+serverless deploy --stage $env --package $CODEBUILD_SRC_DIR/target/$env -r ap-northeast-2
